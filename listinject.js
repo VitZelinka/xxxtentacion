368770072643c952ba88804572a996d602bf5bc8
VítZelinka <xzelin26@stud.fit.vutbr.cz>
2023-07-14 15:59:44 +0200

    added finding rows by id feature

--- listinject.js
+++ listinject.js
@@ -1,9 +1,21 @@
 console.log("XDDXDXD YIPPPPEEEEEE");
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    async function(request, sender, sendResponse) {
         if (request.edit === "startEdit") {
-            console.log("YIPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+            console.log(FindByID(await chrome.storage.local.get(["id"])));
         }
     }
 );
+
+function FindByID(stored_id) {
+    let ids = document.getElementsByClassName("w35 center");
+    for (let i = 0; i < ids.length; i++) {
+        const row_id = ids[i].childNodes[0].innerHTML;
+        console.log(row_id, stored_id.id);
+        if (row_id == stored_id.id) {
+            return ids[i].parentElement;
+        }
+    }
+    return false;
+}
